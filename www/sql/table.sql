442d74e42e5c94009c384dddd0658baa38a650a9
freejoe76 <jmurphy@denverpost.com>
2017-04-06 17:46:41 -0400

    Forgot that

--- www/sql/table.sql
+++ www/sql/table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `kd_TEAMNAME` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` int(10) DEFAULT NULL,
   `year` int(4) DEFAULT NULL,
   `section` varchar(30) DEFAULT NULL,
