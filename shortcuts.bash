e1caea0545a7a51a428524b3d9b6cbed418016c4
Kelli Parker <iamkelliparker@gmail.com>
2017-09-27 14:12:04 -0400

    share image, shortcuts and tiny css tweak

---
+++ shortcuts.bash
@@ -0,0 +1,5 @@
+alias deployqa='cd ~/work/keep-em-dump-em/www/; scp -r trump-white-house qa:/apps/poll/keep-em-dump-em/'
+
+alias deployprod='cd ~/work/keep-em-dump-em/www/; scp -r trump-white-house prod:/apps/poll/keep-em-dump-em/'
+
+alias daww='deployqa; deployprod'
