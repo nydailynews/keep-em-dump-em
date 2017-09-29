74a884f0ef66be82339f303f4fa18ef4737993fb
Kelli Parker <iamkelliparker@gmail.com>
2017-09-29 13:15:52 -0400

    small change to the Yankees json file

--- shortcuts.bash
+++ shortcuts.bash
@@ -1,5 +1,5 @@
-alias deployqa='cd ~/work/keep-em-dump-em/www/; scp -r trump-white-house qa:/apps/poll/keep-em-dump-em/'
+alias deployqa='cd ~/work/keep-em-dump-em/www/; scp -r yankees-2017 qa:/apps/poll/keep-em-dump-em/'
 
-alias deployprod='cd ~/work/keep-em-dump-em/www/; scp -r trump-white-house prod:/apps/poll/keep-em-dump-em/'
+alias deployprod='cd ~/work/keep-em-dump-em/www/; scp -r yankees-2017 prod:/apps/poll/keep-em-dump-em/'
 
 alias daww='deployqa; deployprod'
