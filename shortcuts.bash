559ac72f2bb72bf28a2776da718925a984ee0e02
Joe Murphy <joe.murphy@gmail.com>
2017-11-17 09:30:06 -0500

    Delete shortcuts.bash

--- shortcuts.bash
+++
@@ -1,5 +0,0 @@
-alias deployqa='cd ~/work/keep-em-dump-em/www/; scp -r yankees-2017 qa:/apps/poll/keep-em-dump-em/'
-
-alias deployprod='cd ~/work/keep-em-dump-em/www/; scp -r yankees-2017 prod:/apps/poll/keep-em-dump-em/'
-
-alias daww='deployqa; deployprod'
